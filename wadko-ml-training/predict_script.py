e5132a24b7eeb512f548e6f874be7261ea10fc7e
Kamil Piekorz <115660494+ThePiekorz@users.noreply.github.com>
2023-11-22 13:20:50 +0100

    Update predict_script.py

--- wadko-ml-training/predict_script.py
+++ wadko-ml-training/predict_script.py
@@ -1,15 +1,32 @@
 from fastai.vision.all import *
 import PIL
 
-# Załaduj model
-model_export = load_learner('export.pkl')
+def predict_image(image_path, model_path='export.pkl'):
+    """
+    Predicts the class of an image using a pre-trained fastai vision model.
 
-# Ścieżka do nowego obrazu
-image_path = 'kot.jpg'
+    Parameters:
+    - image_path (str): The path to the image file to be classified.
+    - model_path (str, optional): The path to the pre-trained model file (export.pkl).
+                                 Defaults to 'export.pkl' in the current directory.
 
-# Wczytaj nowy obraz
-img = PIL.Image.create(image_path)
+    Returns:
+    - str: The predicted class of the input image.
 
-# Przewidywanie
-pred, _, _ = model_export.predict(img)
-print(pred)
+    Example:
+    >>> image_path = 'kot.jpg'
+    >>> prediction = predict_image(image_path)
+    >>> print(f'Prediction: {prediction}')
+    """
+    # Load the model
+    model_export = load_learner(model_path)
+
+    # Load the image
+    img = PIL.Image.create(image_path)
+
+    # Make a prediction
+    pred, _, _ = model_export.predict(img)
+    
+    return pred
+
+if __name__ == "__main__":
